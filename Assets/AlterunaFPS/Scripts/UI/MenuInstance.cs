1f85a6112bb865e90b75b7e1142cf5eb477415e6
Виктор Перов <73wwz@bk.ru>
2023-09-15 17:33:00 +0300

    Add multiplayer fps asset

---
+++ Assets/AlterunaFPS/Scripts/UI/MenuInstance.cs
@@ -0,0 +1,39 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace AlterunaFPS
+{
+	public class MenuInstance : MonoBehaviour
+	{
+		public static GameObject Instance;
+
+		private void Awake()
+		{
+			Instance = gameObject;
+			gameObject.SetActive(false);
+		}
+
+		private void OnDisable()
+		{
+			Cursor.visible = false;
+			Cursor.lockState = CursorLockMode.Locked;
+		}
+
+		public void OnEnable()
+		{
+			Cursor.visible = true;
+			Cursor.lockState = CursorLockMode.None;
+		}
+		
+		public void LoadScene(int sceneIndex)
+		{
+			SceneManager.LoadScene(sceneIndex);
+		}
+		
+		public void LoadScene(string sceneName)
+		{
+			SceneManager.LoadScene(sceneName);
+		}
+	}
+}
